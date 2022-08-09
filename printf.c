97ddae97676b1ab7b08ec6097a363d606816a852
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-09 14:00:16 -0700

    run

--- printf.c
+++ printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 	length = strlen(format) - 1;
 	if (format == NULL)
 		return (0);
-	for (i = 0; i < length; i++)
+	for (i = 0; i <= length; i++)
 	{
 		if (format[i] == '%' && (format[i + 1] != 'd' && format[i + 1] != 's' &&
 		format[i + 1] != 'c'))
