11376da49019e6be488f3af9eba5e013c035dd67
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-11 07:18:18 -0700

    hotFix

--- printf.c
+++ printf.c
@@ -10,30 +10,51 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int i = 0, length;
+	va_list valist;
+	int i = 0;
+	int length = 0;
 
-	length = strlen(format) - 1;
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	va_start(ap, format);
-	while (i < length)
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] != '\0')
-			{
 				if (format[i + 1] == 's')
-					printf("%s", va_arg(ap, char *));
+					fprintf(stdout, "%s", va_arg(valist, char *));
 				if (format[i + 1] == 'c')
-					printf("%c", va_arg(ap, int));
+					fprintf(stdout, "%c", va_arg(valist, int));
+				if (format[i + 1] == 'd')
+					fprintf(stdout, "%d", va_arg(valist, int));
 				i += 1;
-			}
 		}
 		else
-			putchar(format[i]);
+			if (format[i])
+				fprintf(stdout, "%c", format[i]);
 		i++;
+		length++;
 	}
-	va_end(ap);
+	va_end(valist);
 	return (length);
+}
+
+int main(void)
+{
+	int len, len2, len3, len4;
+	
+	len = _printf("Let's print a simple sentence.\n");
+	len2 = printf("Let's print a simple sentence.\n");
+	len3 = _printf("%c", 'S');
+	len4 =  printf("%c", 'S');
+	fflush(stdout);
+	if ((len != len2) || (len3 != len4)) 
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
 }
